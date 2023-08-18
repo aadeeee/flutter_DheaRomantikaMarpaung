42f126b2ef443961761b5c3b2c879d54e1371571
None <dhea.r.marpaung@gmail.com>
2023-08-18 21:52:58 +0700

    add tugas prioritas 2

---
+++ 03_Fundamental Dart/Praktikum/prioritas_2.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+import 'dart:math';
+//soal no 1
+class GabungString {
+  String string1;
+  String string2;
+  String string3;
+  GabungString(this.string1, this.string2, this.string3);
+  //hasil penggabungan string
+  String result() {
+    String hasil = string1+ " " + string2 + " " + string3;
+    return hasil;
+  }
+}
+
+//soal no 2
+class Tabung{
+  double jariJari;
+  double tinggi;
+  Tabung(this.jariJari, this.tinggi);
+  //rumus volume lingkaran
+  double volumeTabung() {
+    return pi * jariJari * jariJari * tinggi;
+  }
+}
+
+void main() {
+  print("***Tekan Enter jika anda tidak ingin menjalankan program***");
+  //String
+  String inputString(String message){
+    stdout.write(message);
+    return stdin.readLineSync() ?? "";
+  }
+  print("Hasil penggabungan string");
+  var str1= inputString("Masukkan teks 1: ");
+  var str2= inputString("Masukkan teks 2: ");
+  var str3= inputString("Masukkan teks 3: ");
+  print(GabungString(str1, str2, str3).result());
+  print("=========");
+  double inputDouble(String message) {
+    stdout.write(message);
+    return double.tryParse(stdin.readLineSync() ?? '') ?? 0.0;
+  }
+  // Tabung
+  print("Tabung\n");
+  var jariJari = inputDouble("Masukkan jumlah jari-jari tabung: ");
+  var tinggi = inputDouble("Masukkan tinggi tabung: ");
+  var soalTabung = Tabung(jariJari, tinggi);
+  print("Volume Tabung adalah ${soalTabung.volumeTabung()} cm");
+}

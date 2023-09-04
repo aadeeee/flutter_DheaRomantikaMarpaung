5c9603dafcf6e4887770354bc4f6a238b2adde75
None <dhea.r.marpaung@gmail.com>
2023-09-04 21:59:35 +0700

    add some files to 09_Installation Command Line Interface and Package Management in Flutter

---
+++ 09_Installation Command Line Interface and Package Management in Flutter/Praktikum/newapp/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:newapp/pages/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+            appBarTheme: AppBarTheme(color: Colors.amberAccent),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.amberAccent)))),
+        home: MyHome());
+  }
+}
+
---
+++ 09_Installation Command Line Interface and Package Management in Flutter/Praktikum/newapp/lib/pages/home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatefulWidget {
+  const MyHome({
+    super.key,
+  });
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Halo Alterra"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Nama saya adalah Dhea Romantika Marpaung',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            ElevatedButton(onPressed: (){}, child: Text("Button"))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
